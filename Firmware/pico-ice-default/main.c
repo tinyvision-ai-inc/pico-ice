5f37b7991cc652b30b3fd7f52b38341ddd94238f
Josuah Demangeon <me@josuah.net>
2023-09-01 12:32:19 +0200

    Firmware: add a shell and the SPI bridge

--- Firmware/pico-ice-default/main.c
+++ Firmware/pico-ice-default/main.c
@@ -36,8 +36,81 @@
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
 
+#define DOC_FORWARD_SPI \
+"https://pico-ice.tinyvision.ai/ice_usb.html#usb-spi-fpgasramflash-forwarding"
+
+#define DOC_DEFAULT_FIRMWARE \
+"https://github.com/tinyvision-ai-inc/pico-ice/tree/main/Firmware/pico-ice-default"
+
+// for repl_ungetchar() to take back the last character from repl_getchar():
+int repl_last_char;
+bool repl_last_held;
+
+static int repl_getchar(void) {
+    int c;
+
+    if (repl_last_held) {
+        repl_last_held = false;
+        return repl_last_char;
+    }
+
+    // busy-wait with a slow delay: this is for interactive I/O, no need to be fast
+    while ((c = getchar_timeout_us(10000)) == PICO_ERROR_TIMEOUT) {
+        // call tud_task() since we are blocking, similar to preemptive multitasking
+        tud_task();
+    }
+
+    if (c == '\r' || c == '\n') {
+        printf("\r\n");
+    } else {
+        putchar(c);
+    }
+    return c;
+}
+
+static void repl_ungetchar(int c)
+{
+    assert(!repl_last_held);
+    repl_last_char = c;
+    repl_last_held = true;
+}
+
+static inline bool repl_parse_error(char *msg, char c)
+{
+    // reset whatever was being input
+    repl_last_held = false;
+
+    printf("\nerror: expected %s got '%c'\n", msg, c);
+    return false;
+}
+
+static bool repl_parse_newline(void)
+{
+    int c;
+
+    switch (c = repl_getchar()) {
+    case '\r':
+    case '\n':
+    case EOF:
+        return true;
+    default:
+        repl_ungetchar(c);
+        return repl_parse_error("newline", c);
+    }
+}
+
+static void repl_command_version(void) {
+    if (!repl_parse_newline()) {
+        return;
+    }
+    printf("pico-ice-sdk %s\r\n", VERSION);
+}
+
 int main(void) {
-    // Enable the UART
+    // Enable USB-CDC #0 (serial console)
+    stdio_init_all();
+
+    // Enable the physical UART
     uart_init(uart0, 115200);
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
@@ -54,6 +127,31 @@ int main(void) {
 
     while (true) {
         tud_task();
+
+        printf("\x1b[1mpico-ice>\x1b[m ");
+
+        switch (repl_getchar()) {
+        case 'v':
+            repl_command_version();
+            break;
+        default:
+            printf("\r\n");
+            printf("pico-ice default firmware\r\n", VERSION);
+            printf("   %s\r\n", DOC_DEFAULT_FIRMWARE);
+            printf("\r\n");
+            printf("Serial port #0 - this shell, with commands:\r\n");
+            printf("  v - print pico-ice-sdk version\r\n");
+            printf("\r\n");
+            printf("Serial port #1 - forwarding to UART\r\n");
+            printf("  UART TX on RP0 = ICE27\r\n");
+            printf("  UART RX on RP1 = ICE25\r\n");
+            printf("\r\n");
+            printf("Serial port #2 - forwarding to SPI:\r\n");
+            printf("  %s\r\n", DOC_FORWARD_SPI);
+            printf("\r\n");
+            break;
+        }
     }
+
     return 0;
 }
