120b80528c92672168eb1ad6341a0d373d324f64
vr2045 <29843211+vr2045@users.noreply.github.com>
2025-04-27 15:58:03 -0700

    Changed auto-detect to waiting

--- Firmware/pico-ice-test-jig-mpy/mfg_pico-ice.ps1
+++ Firmware/pico-ice-test-jig-mpy/mfg_pico-ice.ps1
@@ -8,25 +8,30 @@ while ($DeviceId -eq  $null) {
 echo "Flashing..."
 Copy-Item "pico_ice.uf2" -Destination $DeviceId
 echo "Done, Waiting Until Disk shows up again, You may need to unplug and re-plug the device if windows decided so"
-$DeviceId = $null
-while ($DeviceId -eq  $null) {
-	Start-Sleep -Seconds 1
-	$DeviceId = Get-WmiObject Win32_logicaldisk| ? {$_.DriveType -eq 2} | Select-Object -Property DeviceId
-	$DeviceId = $DeviceId.DeviceId
-}
+Start-Sleep -Seconds 2
+
+#$DeviceId = $null
+#while ($DeviceId -eq  $null) {
+#	Start-Sleep -Seconds 1
+#	$DeviceId = Get-WmiObject Win32_logicaldisk| ? {$_.DriveType -eq 2} | Select-Object -Property DeviceId
+#	$DeviceId = $DeviceId.DeviceId
+#}
 echo "Copying test.bit"
 mpremote cp test_pico_ice.bit :test.bit
+Start-Sleep -Seconds 1
+
 echo "Running tests"
 mpremote run run_tests_rp2040.py
 if($?) {
 	echo "Tests Successful... Deleting files and flashing factory FPGA firmware"
 	mpremote rm :test.bit
 	mpremote cp final.bit :
+	Start-Sleep -Seconds 1
 	mpremote run flash_final_rp2040.py
 	mpremote rm :final.bit
 	mpremote cp main_rp2040.py :main.py
 }
 else
 {
-	echo "Tests unSuccessful..."
+	echo "*** Tests failed! ***"
 }
